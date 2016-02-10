9681bb2560d9199484de7a1b227665759cd9640a
Michael Bisbjerg <michael@mbwarez.dk>
2016-02-10 21:00:20 +0100

    Updates based on review from Chocolately

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,14 +3,12 @@
 $packageName= 'xencenter'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://downloadns.citrix.com.edgesuite.net/10341/XenServer-6.5.0-SP1-XenCenterSetup.exe'
-$url64      = $url
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   silentArgs    = "/i /q /Log /LogFile `"$env:TEMP\chocolatey\$($packageName)\$($packageName).Install.log`""
   validExitCodes= @(0, 3010, 1641)
@@ -18,8 +16,6 @@ $packageArgs = @{
   softwareName  = 'Citrix XenCenter'
   checksum      = 'ac655ebe8a9cb05f042af2a3fb74e662cf83b8de'
   checksumType  = 'sha1'
-  checksum64    = 'ac655ebe8a9cb05f042af2a3fb74e662cf83b8de'
-  checksumType64= 'sha1'
 }
 
 Install-ChocolateyPackage @packageArgs
